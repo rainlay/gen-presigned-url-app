bd5eb96cc1f5327ded10605b720dbec03e41bb4f
Rain <rainlay@gmail.com>
2021-04-21 17:52:56 +0800

    wip

--- handler.js
+++ handler.js
@@ -1,15 +1,29 @@
 import AWS from "aws-sdk";
+import cuid from "cuid";
 
 const serverless = require("serverless-http");
 const express = require("express");
 const app = express();
+const awsKey = process.env.AWS_ACCESS_KEY_ID
+const awsSecret = process.env.AWS_SECRET_ACCESS_KEY
 
-function initMultipartUpload() {
+async function initMultipartUpload() {
+  const bucket = "indochat-assets"
   const s3 = new AWS.S3({
-    accessKeyId: "",
-    secretAccessKey: "",
-    sessionToken: ""
+    accessKeyId: awsKey,
+    secretAccessKey: awsSecret,
+    sessionToken: `session-${cuid()}`
   })
+
+  const params = {
+    Bucket: "indochat_asset",
+    // 上傳的檔案路徑名稱
+    Key: "test-presigned-upload"
+  }
+
+  const res = await s3.createMultipartUpload(params).promise()
+
+  console.log(res)
 }
 
 app.get("/", (req, res, next) => {
